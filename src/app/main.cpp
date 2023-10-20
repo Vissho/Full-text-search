03166fb431d68ffc189b65fb0a2ac2ee9459d1cc
Ilya Lisovitskiy <il.lis@mail.ru>
2023-10-20 14:17:36 +0700

    Add try/catch in main

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -14,25 +14,30 @@ int main(int argc, char** argv)
             "id", "id", cxxopts::value<size_t>());
     options.parse_positional({"id", "text"});
 
-    auto result = options.parse(argc, argv);
-    const fts::Words text = result["text"].as<fts::Words>();
-    const size_t document_id = result["id"].as<size_t>();
+    try {
+        auto result = options.parse(argc, argv);
+        const fts::Words text = result["text"].as<fts::Words>();
+        const size_t document_id = result["id"].as<size_t>();
 
-    fts::IndexBuilder builder;
-    builder.add_document(document_id, text);
+        fts::IndexBuilder builder;
+        builder.add_document(document_id, text);
 
-    const fts::Words text1 = {"The", "Matrix", "Reloaded"};
-    const size_t document_id1 = 200305;
-    builder.add_document(document_id1, text1);
+        const fts::Words text1 = {"The", "Matrix", "Reloaded"};
+        const size_t document_id1 = 200305;
+        builder.add_document(document_id1, text1);
 
-    const fts::Words text2 = {"The", "Matrix", "Revolutions"};
-    const size_t document_id2 = 200311;
-    builder.add_document(document_id2, text2);
+        const fts::Words text2 = {"The", "Matrix", "Revolutions"};
+        const size_t document_id2 = 200311;
+        builder.add_document(document_id2, text2);
 
-    builder.print_index();
+        builder.print_index();
 
-    const fts::IndexWriter writer(builder.get_index(), "index.txt");
-    writer.write_text();
+        const fts::IndexWriter writer(builder.get_index(), "index.txt");
+        writer.write_text();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
